bba0b9dd2ee749c4cbf1bf3ed377ee0f7d928d1a
Mike <michaelmamaye@gmail.com>
2024-05-07 21:18:54 +0300

    updated the dashboard ui and some other updates

--- src/app/NextAuthAndStoreProvider.tsx
+++ src/app/NextAuthAndStoreProvider.tsx
@@ -3,18 +3,38 @@
 import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { initGammodaState } from "@/redux/store";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { NextFont } from "next/dist/compiled/@next/font";
 
 type Props = {
   children?: React.ReactNode;
   session: any;
+  font: NextFont;
 };
 
-export const NextAuthAndStoreProvider = ({ children, session }: Props) => {
+export const NextAuthAndStoreProvider = ({
+  children,
+  session,
+  font,
+}: Props) => {
   const store = initGammodaState();
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#34A858",
+      },
+    },
+    typography: {
+      fontFamily: font.style.fontFamily,
+    },
+  });
 
   return (
     <SessionProvider session={session}>
-      <Provider store={store}>{children}</Provider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </Provider>
     </SessionProvider>
   );
 };
---
+++ src/components/dashboard/PieChartGraph.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import React from "react";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  Label,
+} from "recharts";
+
+const PieChartGraph = ({ paidMembers = 700, unpaidMembers = 2500 }) => {
+  const totalCustomers = paidMembers + unpaidMembers;
+
+  const data = [
+    { name: "Paid Members", value: paidMembers, color: "#34A858" },
+    { name: "Unpaid Members", value: unpaidMembers, color: "#BCE7C9" },
+  ];
+
+  const CustomLabel = ({ viewBox, labelText, value }: any) => {
+    const { cx, cy } = viewBox;
+    return (
+      <g style={{ boxShadow: "10px 10px 10px 0px rgba(0, 0, 0, 0.5)" }}>
+        <text
+          x={cx}
+          y={cy}
+          className="recharts-text recharts-label"
+          textAnchor="middle"
+          dominantBaseline="central"
+          alignmentBaseline="middle"
+          fill="#555555"
+          fontSize="28"
+          fontWeight="800"
+        >
+          {value}
+        </text>
+        <text
+          x={cx}
+          y={cy + 20}
+          className="recharts-text recharts-label"
+          textAnchor="middle"
+          dominantBaseline="central"
+          alignmentBaseline="middle"
+          fontSize="15"
+          fill="#555555"
+        >
+          {labelText}
+        </text>
+      </g>
+    );
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart startAngle={180}>
+        <defs>
+          <radialGradient id="shadow" cx="50%" cy="50%" r="80%">
+            <stop offset="10%" stopColor="rgba(0, 0, 0, 0.5)" />
+            <stop offset="90%" stopColor="rgba(0, 0, 0, 0)" />
+          </radialGradient>
+        </defs>
+        <Pie
+          data={data}
+          dataKey="value"
+          cx="50%"
+          cy="50%"
+          innerRadius={"50%"}
+          outerRadius={'100%'}
+          paddingAngle={0}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} />
+          ))}
+          <Label
+            content={<CustomLabel labelText="Members" value={totalCustomers} />}
+            position="center"
+          />
+        </Pie>
+        <Tooltip />
+        <Legend
+          layout="vertical"
+          align="right"
+          verticalAlign="bottom"
+          wrapperStyle={{ right: 20, bottom: 20 }}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default PieChartGraph;
