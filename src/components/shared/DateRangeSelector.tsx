2b082ddf9841fc449c31c1cb245021b0033dc1a6
Mike <michaelmamaye@gmail.com>
2024-05-28 23:34:42 +0300

    Adjust date range picker colors and dashboard side lists

--- src/components/shared/DateRangeSelector.tsx
+++ src/components/shared/DateRangeSelector.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { addDays } from "date-fns";
 import { useState } from "react";
@@ -5,24 +7,42 @@ import { DateRangePicker } from "react-date-range";
 import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 
-export default function BasicRangeShortcuts() {
+export default function BasicRangeShortcuts({
+  setDateFilter,
+  dateFilter,
+}: {
+  setDateFilter: any;
+  dateFilter: any;
+}) {
   const [state, setState] = useState<any>([
     {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
+      startDate: dateFilter?.startDate ? dateFilter?.startDate : new Date(),
+      endDate: dateFilter?.endDate
+        ? dateFilter?.endDate
+        : addDays(new Date(), 7),
       key: "selection",
     },
   ]);
+
+  React.useEffect(() => {
+    setDateFilter({
+      startDate: state[0].startDate,
+      endDate: state[0].endDate,
+    });
+  }, [state]);
+
   return (
     <div className="overflow-x-auto">
       <DateRangePicker
         onChange={(item) => setState([item.selection])}
-        color="#F1F1F1"
+        color="#34A858"
         className="border-primaryColor outline-primaryColor"
         moveRangeOnFirstSelection={false}
         months={2}
         ranges={state}
         direction="horizontal"
+        inputRanges={[]}
+        rangeColors={['#34a858', '#34a858', '#34a858']}
       />
     </div>
   );
