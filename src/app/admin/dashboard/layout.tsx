db3630ccae8ec1c0b3819f757fdf08e548ac5a33
Mike <michaelmamaye@gmail.com>
2024-06-15 20:43:53 +0300

    fixed reload issues

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -14,9 +14,54 @@ export default async function AdminLayout({
     redirect("/login");
   }
 
+  const menuItems = [
+    {
+      name: "Dashboard",
+      link: "/admin/dashboard",
+      activeIcon: "/icons/dashboardactive.svg",
+      icon: "/icons/dashboard.svg",
+      isAccessible: Boolean(session?.user?.role !== UserRole.Admin),
+    },
+    {
+      name: "Members List",
+      link: "/admin/dashboard/members",
+      activeIcon: "/icons/membersactive.svg",
+      icon: "/icons/members.svg",
+      isAccessible: true,
+    },
+    {
+      name: "General Donation",
+      link: "/admin/dashboard/donations",
+      activeIcon: "/icons/donationactive.svg",
+      icon: "/icons/donation.svg",
+      isAccessible: Boolean(session?.user?.role !== UserRole.Admin),
+    },
+    {
+      name: "CMS",
+      link: "/admin/dashboard/cms",
+      activeIcon: "/icons/cmsactive.svg",
+      icon: "/icons/cms.svg",
+      isAccessible: Boolean(session?.user?.role !== UserRole.Admin),
+    },
+    {
+      name: "Auction",
+      link: "/admin/dashboard/auction",
+      activeIcon: "/icons/auctionactive.svg",
+      icon: "/icons/auction.svg",
+      isAccessible: Boolean(session?.user?.role !== UserRole.Admin),
+    },
+    {
+      name: "SMS",
+      link: "/admin/dashboard/sms",
+      activeIcon: "/icons/smsactive.svg",
+      icon: "/icons/sms.svg",
+      isAccessible: Boolean(session?.user?.role !== UserRole.Admin),
+    },
+  ];
+
   return (
     <div className="flex flex-row h-screen w-screen relative">
-      <Sidebar />
+      <Sidebar menuItems={menuItems} />
       <div className="flex-1 max-w-full bg-[#f5f5f5]">{children}</div>
     </div>
   );
