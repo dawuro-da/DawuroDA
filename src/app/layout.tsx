8b38229f5f6a6211cf0a815676f8d2acc66d548d
Mike <michaelmamaye@gmail.com>
2024-06-15 22:48:02 +0300

    minor session fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={lexend.className}>
-        <NextAuthAndStoreProvider session={session} font={lexend}>
+        <NextAuthAndStoreProvider font={lexend}>
           {children}
         </NextAuthAndStoreProvider>
       </body>
