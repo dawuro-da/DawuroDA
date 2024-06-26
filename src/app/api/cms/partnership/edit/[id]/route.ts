6dffbeb43346e03449dbea5ec1ee1993fcb76c96
Mike <michaelmamaye@gmail.com>
2024-06-26 11:02:29 +0300

    updated session issues

--- src/app/api/cms/partnership/edit/[id]/route.ts
+++ src/app/api/cms/partnership/edit/[id]/route.ts
@@ -8,10 +8,7 @@ import { uploadFile } from "@/util/uploadFile";
 export async function POST(req: Request, context: { params: { id: string } }) {
   const session = await getServerSession(OPTIONS);
   if (!session?.user?.id || session?.user.role === UserRole.Member) {
-    return NextResponse.json(
-      { success: false, error: "Unauthorized user" },
-      { status: 401 }
-    );
+    return NextResponse.redirect("/gaadmin/login", 401)
   }
   const partnershipId = context.params.id;
   const formData = await req.formData();
