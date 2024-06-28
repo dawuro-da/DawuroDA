ed0055f0a1e9d16a1a653753bdd742b541d249af
Mike <michaelmamaye@gmail.com>
2024-06-28 17:31:12 +0300

    Fix footer and partners section

--- src/landingPage/homepage/sections/Partners.tsx
+++ src/landingPage/homepage/sections/Partners.tsx
@@ -14,13 +14,11 @@ const Partners = () => {
       <h2 className="font-bold lg:text-4xl md:text-2xl text-xl mb-10 text-center">
         Our Partners
       </h2>
-      <marquee>
-        <div className="flex lg:space-x-20 space-x-8  hiddenscrollbar md:w-2/4 mx-auto ">
-          {PartnerItems.map((item, id) => (
-            <Image src={item} height={80} width={80} alt="" key={id} />
-          ))}
-        </div>
-      </marquee>
+      <div className="flex lg:space-x-20 space-x-8  hiddenscrollbar md:w-2/4 mx-auto ">
+        {PartnerItems.map((item, id) => (
+          <Image src={item} height={80} width={80} alt="" key={id} />
+        ))}
+      </div>
 
       <div className="text-[#FFFFFF] pt-32 text-left px-11 mt-20 lg:bg-[url('/images/partnerbg.svg')] bg-[url('/images/partnerbg2.svg')] lg:h-80 h-96 w-4/5 mx-auto bg-cover">
         <h3 className="lg:text-4xl text-lg font-bold mb-3">
