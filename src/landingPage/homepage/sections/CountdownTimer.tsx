568425e911222fbe5d666f72f0f807f5dc46002d
Yishak Kidanu <yisak04@gmail.com>
2024-06-28 09:15:38 +0300

    UI implemtation

---
+++ src/landingPage/homepage/sections/CountdownTimer.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+const CountdownTimer = () => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(`2024-07-15T09:00:00`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState<any>(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div className="flex items-center flex-row justify-center md:space-x-11 space-x-8 w-4/5 mx-auto bg-[#13A6D9] text-white p-6">
+      {Object.keys(timeLeft).map((interval) => (
+        <div key={interval} className="text-center w-10">
+          <p className="text-lg font-bold text-white">{timeLeft[interval]}</p>
+          <p className="text-sm capitalize text-white">{interval}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CountdownTimer;
