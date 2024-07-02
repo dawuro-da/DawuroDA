72238df2cd213a6c914c198dcb21afa8bb9f5aca
Mike <michaelmamaye@gmail.com>
2024-07-02 19:32:49 +0300

    Add member login page and homepage updates

--- src/landingPage/homepage/Home.tsx
+++ src/landingPage/homepage/Home.tsx
@@ -1,6 +1,6 @@
 "use client";
 import HeroSection from "./sections/HeroSection";
-import SupportRedSection from "./sections/SupportRedSection";
+import CampaignSection from "./sections/CampaignSection";
 import AboutSection from "./sections/AboutSection";
 import DevelopmentInitiatives from "./sections/DevelopmentInitiatives";
 import FAQ from "./sections/FAQ";
@@ -13,9 +13,9 @@ const Homepage = () => {
   return (
     <div className="w-full h-screen flex flex-col overflow-y-auto hiddenscrollbar">
       <HeroSection />
-      <SupportRedSection />
       <AboutSection />
       <DevelopmentInitiatives />
+      <CampaignSection />
       <Partners />
       <LatestNews />
       <WhatsHappening />
