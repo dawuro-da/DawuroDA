87f2148d6fecd87c943b494ba94950b2a8b3ff99
Yishak Kidanu <yisak04@gmail.com>
2024-07-03 06:16:20 +0300

    Add vacancy/job listing pages

---
+++ src/landingPage/jobpage/Jobs.tsx
@@ -0,0 +1,18 @@
+import Naviagtion from "../navigation/Navigation";
+import { JobData } from "./sections/JobData";
+import JobPage from "./sections/JobPage";
+
+const Jobs = () => {
+  return (
+    <div className="bg-[#F5F5F5] min-h-screen flex flex-col">
+      <div className="z-40 absolute top-0 w-full">
+        <Naviagtion />
+      </div>
+      <div className="w-4/5 mx-auto lg:mt-[180px] mt-[100px] mb-16">
+        <JobPage job={JobData}/>
+      </div>
+    </div>
+  );
+};
+
+export default Jobs
