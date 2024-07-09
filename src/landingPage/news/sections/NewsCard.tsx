3b994fdfb1e52fcd4fde9228180d75183c69c3dc
Mike <michaelmamaye@gmail.com>
2024-07-09 20:21:31 +0300

    Refine news grid cards

--- src/landingPage/news/sections/NewsCard.tsx
+++ src/landingPage/news/sections/NewsCard.tsx
@@ -43,7 +43,7 @@ const NewsCard = () => {
       <h2 className="font-bold lg:text-4xl text-lg text-center">News</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 p-4 md:mt-20 mt-8">
         <div
-          className="relative lg:col-span-1 lg:block hidden cursor-pointer"
+          className="relative lg:col-span-1 lg:block hidden cursor-pointer group"
           onClick={() => router.push("/news/news-detail")}
         >
           <Image
@@ -54,7 +54,7 @@ const NewsCard = () => {
             className="w-[85%]"
           />
           <div className="absolute bottom-12 text-white left-7 text-left">
-            <h2 className="text-2xl font-bold mb-3">{newsItems[0].title}</h2>
+            <h2 className="text-2xl font-bold mb-3 group-hover:underline">{newsItems[0].title}</h2>
             <div className="flex items-center space-x-3">
               <Image
                 src="/images/calendar2.svg"
@@ -69,7 +69,7 @@ const NewsCard = () => {
           </div>
         </div>
         <div className="grid grid-cols-1 lg:col-span-1 md:col-span-7 gap-0">
-          <div className="relative lg:col-span-1 lg:block hidden mb-5">
+          <div className="relative lg:col-span-1 lg:block hidden mb-5 group cursor-pointer">
             <Image
               src={newsItems[4].imgSrc}
               alt=""
@@ -77,8 +77,10 @@ const NewsCard = () => {
               height={20}
               className="lg:w-full"
             />
-            <div className="absolute bottom-12 text-white left-7 text-left">
-              <h2 className="text-2xl font-bold mb-3">{newsItems[0].title}</h2>
+            <div className="absolute bottom-12 text-white left-7 text-left cursor-pointer">
+              <h2 className="group-hover:underline text-2xl font-bold mb-3">
+                {newsItems[0].title}
+              </h2>
               <div className="flex items-center space-x-3">
                 <Image
                   src="/images/calendar2.svg"
@@ -93,17 +95,21 @@ const NewsCard = () => {
             </div>
           </div>
           {newsItems?.slice(1, 3).map((item, id) => (
-            <div key={id} className="flex flex-col items-start">
+            <div
+              key={id}
+              className="flex flex-col items-start cursor-pointer group"
+              onClick={() => router.push("/news/news-detail")}
+            >
               <div className="flex flex-row text-start space-x-6 lg:mb-5 mb-4">
                 <Image
                   src={item.imgSrc}
                   alt=""
                   width={20}
                   height={20}
-                  className="lg:w-[41%] w-[38%]"
+                  className="lg:w-[38%] max-h-[200px] w-[38%]"
                 />
                 <div className="mt-2 md:pl-6 pl-0 lg:w-2/4">
-                  <h2 className="md:text-xl text-xs font-bold md:mb-1 mb-2 text-[#1E1E1E]">
+                  <h2 className="group-hover:underline md:text-xl text-xs font-bold md:mb-1 mb-2 text-[#1E1E1E]">
                     {item.title}
                   </h2>
                   <div className="flex items-center space-x-3">
@@ -124,7 +130,11 @@ const NewsCard = () => {
         </div>
         <div className="flex xl:lg:flex-row md:flex-row flex-col col-span-2 xl:lg:mt-16">
           {newsItems?.slice(1, 3).map((item, id) => (
-            <div key={id} className="flex flex-col items-start">
+            <div
+              key={id}
+              className="flex flex-col items-start cursor-pointer group"
+              onClick={() => router.push("/news/news-detail")}
+            >
               <div className="flex flex-row text-start space-x-6 lg:mb-5 mb-4">
                 <Image
                   src={item.imgSrc}
@@ -134,7 +144,7 @@ const NewsCard = () => {
                   className="lg:w-[41%] w-[38%]"
                 />
                 <div className="mt-2 md:pl-6 pl-0 lg:w-2/4">
-                  <h2 className="md:text-xl text-xs font-bold md:mb-1 mb-2 text-[#1E1E1E]">
+                  <h2 className="group-hover:underline md:text-xl text-xs font-bold md:mb-1 mb-2 text-[#1E1E1E]">
                     {item.title}
                   </h2>
                   <div className="flex items-center space-x-3">
