325b5a605c5d8eaf720a6ed1c9dbc72f947dc00b
Mike <michaelmamaye@gmail.com>
2024-08-06 22:10:57 +0300

    Add member bid history

--- src/members/components/MemberAuction.tsx
+++ src/members/components/MemberAuction.tsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { Auction } from "@prisma/client";
+import { Auction, Bidder } from "@prisma/client";
 import { getFormattedDate } from "@/util/date";
+import { CircularProgress, Skeleton } from "@mui/material";
 
 const MemberAuction = () => {
   const router = useRouter();
-  const [auctions, setAuctions] = useState<Auction[]>();
+  const [auctions, setAuctions] = useState<
+    (Auction & {
+      totalBidders: number;
+      totalCPO: number;
+      totalDocumentSales: number;
+    })[]
+  >();
+  const [bidders, setBidders] = useState<Bidder[]>();
   const [loading, setLoading] = useState(false);
   const [loadingMore, setLoadingMore] = useState(false);
   const [page, setPage] = useState(1);
@@ -31,23 +39,62 @@ const MemberAuction = () => {
     setLoadingMore(false);
   };
 
+  const fetchUserBids = async () => {
+    try {
+      const res = await axios.post("/api/auction/userBids");
+      if (res.data.success) {
+        setBidders(res.data.value);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     fetchAuctions();
+    fetchUserBids();
   }, [page]);
 
   return (
     <div className="flex flex-col gap-6 w-full mt-10">
-      {auctions?.map((item, index) => (
-        <AuctionCard
-          key={index}
-          startDate={getFormattedDate(item.startDate)}
-          title={item.title}
-          description={item.description}
-          bidder={64}
-          onClick={() => router.push(`/auctions/${item.id}`)}
-          endDate={getFormattedDate(item.endDate)}
-        />
-      ))}
+      {loading ? (
+        <>
+          <Skeleton style={{ width: "100%", height: "200px" }} />
+          <Skeleton style={{ width: "100%", height: "200px" }} />
+        </>
+      ) : (
+        auctions?.map((item, index) => {
+          const bidder = bidders?.filter(
+            (bidder) => bidder.auctionId === item.id
+          );
+          const isApplied = Boolean(bidder?.[0]?.isSubmitted);
+          const isInProgress = Boolean(
+            bidder?.[0]?.hasPaidCPO || bidder?.[0]?.hasPaidNRP
+          );
+
+          return (
+            <AuctionCard
+              key={index}
+              startDate={getFormattedDate(item.startDate)}
+              title={item.title}
+              description={item.description}
+              bidder={item.totalBidders}
+              isApplied={isApplied}
+              isInProgress={isInProgress}
+              onClick={() => router.push(`/auctions/${item.id}`)}
+              endDate={getFormattedDate(item.endDate)}
+            />
+          );
+        })
+      )}
+      <div className="w-full my-20">
+        <div
+          onClick={() => setPage(page + 1)}
+          className="cursor-pointer px-10 border border-[#1E1E1E] w-fit font-light mx-auto h-[50px] flex flex-row items-center justify-center"
+        >
+          {loadingMore ? <CircularProgress className="h-full" /> : "Load More"}
+        </div>
+      </div>
     </div>
   );
 };
