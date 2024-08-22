9a4a9a896bd71ec4b43b0011c98a3be8e0a36c4b
Mike <michaelmamaye@gmail.com>
2024-08-22 14:31:39 +0300

    Tweak phone input and member profile forms

--- src/members/auth/signupSections/PhoneAndPassword.tsx
+++ src/members/auth/signupSections/PhoneAndPassword.tsx
@@ -126,7 +126,6 @@ const PhoneAndPassword = ({
               value: international_phone_regex,
             },
           })}
-          disabled={true}
           variant="outlined"
           className="border-2 rounded-[16px] py-2"
           inputProps={{ style: { padding: 10 } }}
