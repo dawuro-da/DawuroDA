11ab6661788deb753b60dda3f4b4c533dcf95df0
Mike <michaelmamaye@gmail.com>
2026-09-15 23:01:09 +0300

    additional fix

--- src/components/shared/sidebar/Sidebar.tsx
+++ src/components/shared/sidebar/Sidebar.tsx
@@ -63,8 +63,8 @@ const Sidebar = (props: any) => {
                 priority
                 unoptimized
                 alt="DawuroDA logo"
-                height={20}
-                width={20}
+                height={10}
+                width={10}
                 style={{ width: "100%" }}
                 draggable={false}
               />
